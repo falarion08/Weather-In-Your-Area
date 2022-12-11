3a87b541ead716e64e014115aa4d1298194e464d
falarion08 <barrion.ryanonil@gmail.com>
2022-12-11 20:01:52 +0800

    Fixed Main Menu

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+    res.render('index');
+});
+
+app.post('/city', function(req,res){
+    res.render('city');
+});
+
+app.listen(3000,'localhost',function(){
+    console.log('Server started on port 3000...');
+});
